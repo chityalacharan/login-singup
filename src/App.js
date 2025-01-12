7291148429cc8f911ebd340f438ac89761df923b
CHITYALA CHARAN TEJA <chityalacharanteja@gmail.com>
2025-01-12 13:00:46 +0530

    login-singup/README.md

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSingup from './Components/LoginSingup/LoginSingup';
+
+function App() {
+  return (
+    <div>
+      <LoginSingup/>
+    </div>
+  );
+}
+
+export default App;
